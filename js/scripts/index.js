4d9266e1b083f39e974df5f8b43b7f1ca78e6cf9
Renan Domingues <renan.dom.val.sil@gmail.com>
2024-05-12 14:53:20 -0300

    refatoração do código em componentes, necessário arrumar modulos js

--- js/scripts/index.js
+++ js/scripts/index.js
@@ -1,14 +1,53 @@
+function loadComponent(url) {
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erro ao carregar o componente de URL: ${url}`);
+      }
+      return response.text();
+    })
+    .then(html => {
+      document.getElementById("divConteudo").innerHTML = html;
+    })
+    .catch(error => {
+      console.error(`Erro ao carregar o componente de URL: ${url}`, error);
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    var navbarUrl = "components/navbar.html";
+  var navbarUrl = "../html/components/navbar.html";
+  var batalhaUrl = "../../html/components/batalha.html";
+  var desafioUrl = "../../html/components/desafio.html";
+  var sorteUrl = "../../html/components/sorte.html";
+  var personagensUrl = "../../html/components/personagens.html";
 
-    fetch(navbarUrl)
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById("navbarContainer").innerHTML = html;
-        })
-        .catch(error => {
-            console.error("Erro ao carregar o navbar:", error);
-        });
+  fetch(navbarUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro ao carregar o navbar');
+      }
+      return response.text();
+    })
+    .then(html => {
+      document.getElementById("divNavbar").innerHTML = html;
 
-    
+      document.getElementById("linkInstrucoes").addEventListener("click", function() {
+        loadComponent("../html/components/instrucoes.html");
+      });
+      document.getElementById("linkBatalha").addEventListener("click", function() {
+        loadComponent("../html/components/batalha.html");
+      });
+      document.getElementById("linkDesafio").addEventListener("click", function() {
+        loadComponent("../html/components/desafio.html");
+      });
+      document.getElementById("linkSorte").addEventListener("click", function() {
+        loadComponent("../html/components/sorte.html");
+      });
+      document.getElementById("linkPersonagens").addEventListener("click", function() {
+        loadComponent("../html/components/personagens.html");
+      });
+    })
+    .catch(error => {
+      console.error("Erro ao carregar o navbar:", error);
+    });
 });
